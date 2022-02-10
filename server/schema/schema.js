71085c7ce5efa223a944284f8663a8148dc612d4
Mark <learncode23@gmail.com>
2022-02-10 13:56:57 +0000

    Returning Mock Data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,18 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+
+var books = [
+  { name: 'Lessons From The Edge', genre: 'Boigraphy', id: '1' },
+  { name: 'Dune', genre: 'Sci Fi', id: '2' },
+  { name: 'The Hobbit', genre: 'Fantasy', id: '3' },
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
@@ -16,9 +23,9 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
-      reslove(parent, args) {
-        //Database
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(books, { id: args.id });
       },
     },
   },
