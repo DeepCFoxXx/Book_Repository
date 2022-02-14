f42ba953cd351f56e7ea92a6e433e061a7ea8449
Mark <learncode23@gmail.com>
2022-02-14 12:00:56 +0000

    Added Author Type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,24 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 
 var books = [
   { name: 'Lessons From The Edge', genre: 'Boigraphy', id: '1' },
   { name: 'Dune', genre: 'Sci Fi', id: '2' },
   { name: 'The Hobbit', genre: 'Fantasy', id: '3' },
+];
+
+var authors = [
+  { name: 'Aldo Kane', age: 44, id: '1' },
+  { name: 'Frank Herbert', age: 65, id: '2' },
+  { name: 'J.r.r Tolkien', age: 85, id: '3' },
 ];
 
 const BookType = new GraphQLObjectType({
@@ -18,6 +30,15 @@ const BookType = new GraphQLObjectType({
   }),
 });
 
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -26,6 +47,13 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
       },
     },
   },
