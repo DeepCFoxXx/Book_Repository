a39bc06e6364ba18f28dfc3cedfbff354f76d935
Mark <learncode23@gmail.com>
2022-02-16 13:59:12 +0000

    Apollo Set-Up Complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client';
+
+// Components
+import BookList from './components/BookList';
+
+// Apollo Client Component
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/grapql',
+  cache: new InMemoryCache(),
+});
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Hello World</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Hello World</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
